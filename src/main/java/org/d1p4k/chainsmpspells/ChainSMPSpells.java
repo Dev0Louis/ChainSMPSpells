43a30386dc5f33a528a710bf9df7fc02e84eaae0
D1p4k <82884007+D1p4k@users.noreply.github.com>
2022-11-27 14:19:08 +0100

    Update ChainSMPSpells.java

--- src/main/java/org/d1p4k/chainsmpspells/ChainSMPSpells.java
+++ src/main/java/org/d1p4k/chainsmpspells/ChainSMPSpells.java
@@ -3,29 +3,18 @@ package org.d1p4k.chainsmpspells;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.FoodComponents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.PotionItem;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import org.d1p4k.chainsmpspells.knowledge.items.SuicideSpellBook;
 import org.d1p4k.chainsmpspells.packet.c2s.SpellUseS2CPacket;
 import org.d1p4k.chainsmpspells.scheduler.TaskExecutor;
 import org.d1p4k.chainsmpspells.test.command.testcommand;
-import org.d1p4k.nebula.knowledge.SpellKnowledge;
 
 public class ChainSMPSpells implements ModInitializer {
 
     public static MinecraftServer server;
-    public static final Item SUICIDE_SPELL_BOOK = new SuicideSpellBook(new FabricItemSettings().group(ItemGroup.MISC).fireproof());
 
     @Override
     public void onInitialize() {
         SpellUseS2CPacket.register();
-        registerSpellBooks();
         CommandRegistrationCallback.EVENT.register((dispatcher, commandRegistryAccess, registrationEnvironment) -> {
             testcommand.register(dispatcher);
         });
@@ -33,9 +22,5 @@ public class ChainSMPSpells implements ModInitializer {
             ChainSMPSpells.server = server;
         });
         TaskExecutor.init();
-    }
-
-    private void registerSpellBooks() {
-        Registry.register(Registry.ITEM, new Identifier("css", "suicide_spell_book"), SUICIDE_SPELL_BOOK);
     }
 }
