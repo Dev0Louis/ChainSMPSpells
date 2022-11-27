edd841a19e1c629d336d6a7a2c0bb414b2b083c5
D1p4k <82884007+D1p4k@users.noreply.github.com>
2022-11-27 14:28:33 +0100

    Update Pull- and PushSpell to pull/push one Player to the user of the Spell

--- src/main/java/org/d1p4k/chainsmpspells/spell/spells/PullSpell.java
+++ src/main/java/org/d1p4k/chainsmpspells/spell/spells/PullSpell.java
@@ -1,49 +1,36 @@
 package org.d1p4k.chainsmpspells.spell.spells;
 
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import org.d1p4k.nebula.spell.AbstractSpell;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.UUID;
+
+import static org.d1p4k.chainsmpspells.ChainSMPSpells.server;
 
 public class PullSpell extends AbstractSpell {
+    public UUID uuid;
     public static Identifier spellId = new Identifier("css" , "pull");
 
-    public PullSpell(ServerPlayerEntity player, Identifier spellIdentifier, int cost) {
+    public PullSpell(ServerPlayerEntity player, UUID uuid, Identifier spellIdentifier, int cost) {
         super(player, spellIdentifier, cost);
+        this.uuid = uuid;
     }
-    public PullSpell(ServerPlayerEntity player, Identifier spellIdentifier) {
-        this(player, spellIdentifier, 3);
+    public PullSpell(ServerPlayerEntity player, UUID uuid, Identifier spellIdentifier) {
+        this(player, uuid, spellIdentifier, 3);
     }
 
 
     @Override
     public void cast() {
         if(check()) {
-            Box box = player.getBoundingBox().expand(32, 40, 32);
-            List<PlayerEntity> players = getPlayersInBox(box, player.getWorld().getPlayers());
-            players.remove(player);
-            for (PlayerEntity player1 : players) {
-                Vec3d velocity = player.getPos().subtract(player1.getPos());
-                player1.setVelocity(velocity.multiply(30/velocity.lengthSquared()));
-                player1.velocityModified = true;
-            }
+            var pulledPlayer = server.getPlayerManager().getPlayer(uuid);
+            if(pulledPlayer == null)return;
+            Vec3d velocity = player.getPos().subtract(pulledPlayer.getPos()).normalize();
+            pulledPlayer.setVelocity(velocity);
+            pulledPlayer.velocityModified = true;
         }
-    }
-    public List<PlayerEntity> getPlayersInBox(Box box, Collection<? extends PlayerEntity> players) {
-        List<PlayerEntity> playerList = new ArrayList<>();
-        for(PlayerEntity player1 : player.getWorld().getPlayers()) {
-            if(box.contains(player1.getPos())) {
-                playerList.add(player1);
-            }
-        }
-        return playerList;
     }
     public boolean check() {
         return checkKnowledge() && checkMana();
--- src/main/java/org/d1p4k/chainsmpspells/spell/spells/PushSpell.java
+++ src/main/java/org/d1p4k/chainsmpspells/spell/spells/PushSpell.java
@@ -1,50 +1,38 @@
 package org.d1p4k.chainsmpspells.spell.spells;
 
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import org.d1p4k.nebula.spell.AbstractSpell;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.UUID;
+
+import static org.d1p4k.chainsmpspells.ChainSMPSpells.server;
 
 public class PushSpell extends AbstractSpell {
 
+    public UUID uuid;
     public static Identifier spellId = new Identifier("css" , "push");
 
-    public PushSpell(ServerPlayerEntity player, Identifier spellIdentifier, int cost) {
+    public PushSpell(ServerPlayerEntity player, UUID uuid, Identifier spellIdentifier, int cost) {
         super(player, spellIdentifier, cost);
+        this.uuid = uuid;
     }
-
-    public PushSpell(ServerPlayerEntity player, Identifier spellIdentifier) {
-        this(player, spellIdentifier, 3);
+    public PushSpell(ServerPlayerEntity player, UUID uuid, Identifier spellIdentifier) {
+        this(player, uuid, spellIdentifier, 3);
     }
 
     @Override
     public void cast() {
         if(check()) {
-            Box box = player.getBoundingBox().expand(32, 40, 32);
-            List<PlayerEntity> players = getPlayersInBox(box, player.getWorld().getPlayers());
-            players.remove(player);
-            for (PlayerEntity player1 : players) {
-                Vec3d velocity = player1.getPos().subtract(player.getPos());
-                player1.setVelocity(velocity.multiply(30/velocity.lengthSquared()));
-                player1.velocityModified = true;
-            }
+            var pulledPlayer = server.getPlayerManager().getPlayer(uuid);
+            if(pulledPlayer == null)return;
+            Vec3d velocity = player.getPos().subtract(pulledPlayer.getPos()).normalize().negate();
+            pulledPlayer.setVelocity(velocity);
+            pulledPlayer.velocityModified = true;
         }
     }
-    public List<PlayerEntity> getPlayersInBox(Box box, Collection<? extends PlayerEntity> players) {
-        List<PlayerEntity> playerList = new ArrayList<>();
-        for(PlayerEntity player1 : player.getWorld().getPlayers()) {
-            if(box.contains(player1.getPos())) {
-                playerList.add(player1);
-            }
-        }
-        return playerList;
-    }
+
 
     public boolean check() {
         return checkKnowledge() && checkMana();
