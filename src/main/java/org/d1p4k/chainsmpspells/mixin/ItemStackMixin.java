53d5bf4d6a71ccd473463c81cdfed2dc289934ad
D1p4k <82884007+D1p4k@users.noreply.github.com>
2022-11-27 14:36:01 +0100

    Code Cleanup + Remove unneeded debug message.

--- src/main/java/org/d1p4k/chainsmpspells/mixin/ItemStackMixin.java
+++ src/main/java/org/d1p4k/chainsmpspells/mixin/ItemStackMixin.java
@@ -1,18 +1,12 @@
 package org.d1p4k.chainsmpspells.mixin;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.text.Text;
 import net.minecraft.world.World;
-import org.d1p4k.chainsmpspells.ChainSMPSpells;
 import org.d1p4k.chainsmpspells.accessor.ItemStackJuggernautModeAccessor;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,24 +15,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin implements ItemStackJuggernautModeAccessor {
-
-    @Shadow public abstract Item getItem();
-
     @Shadow public abstract @Nullable NbtCompound getNbt();
-
-
-    @Shadow @Final public static ItemStack EMPTY;
-
-    @Shadow @Final @Deprecated private Item item;
-
     @Shadow public abstract void setCount(int count);
-
-    @Shadow public abstract void inventoryTick(World world, Entity entity, int slot, boolean selected);
-
-    @Inject(method = "postHit", at = @At("HEAD"))
-    public void noDamageMixin(LivingEntity target, PlayerEntity attacker, CallbackInfo ci) {
-        attacker.sendMessage(Text.of(getJuggernautTick() + ""));
-    }
 
 
     @Inject(method = "inventoryTick", at = @At("TAIL"))
