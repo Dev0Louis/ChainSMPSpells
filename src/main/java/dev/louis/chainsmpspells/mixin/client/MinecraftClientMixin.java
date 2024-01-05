f372077413eff1a9466b8eaec56404325bf53a8c
D1p4k <82884007+D1p4k@users.noreply.github.com>
2024-01-05 22:34:39 +0100

    fix refmap false

--- src/main/java/dev/louis/chainsmpspells/mixin/client/MinecraftClientMixin.java
+++ src/main/java/dev/louis/chainsmpspells/mixin/client/MinecraftClientMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = MinecraftClient.class, remap = false)
+@Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
     @Shadow @Nullable public ClientPlayerEntity player;
     @Unique
